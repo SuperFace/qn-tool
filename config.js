2ef49bc86ce4a2d727df8dc28d289821e51ddff9
林小蕾 <linxiaolei@xuetangx.com>
2020-09-09 17:27:52 +0800

    添加注释

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  publicPath: 'https://xxxxx.com/', //CDN域名，最后要有：‘/’
-  accessKey: 'xxxxxxx',//AK
-  secretKey: 'xxxxxx',//SK
+  publicPath: 'https://proxt-cdn.xuetangx.com/', //CDN域名，最后要有：‘/’
+  accessKey: 'XbBjw6RmDf-8tTJ8vwVjI6fR1sOboimKSfnRK6Jf',//AK
+  secretKey: '_7ZPARAS--B5en3Y7qgT6f7XNemQCbR5iybLruqo',//SK
   bucket: 'fe-proxtassets',//Bucket
   zone: 'Zone_z1',//华北
   cover: true
