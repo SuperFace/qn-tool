128047a885a135511eaa74bd5e3b784cc7ee3fc1
林小蕾 <linxiaolei@xuetangx.com>
2020-09-10 19:14:46 +0800

    超大文件夹，分批次上传。

--- config.js
+++ config.js
@@ -1,8 +1,24 @@
+// module.exports = {
+//   publicPath: 'https://xxxxx.com/', //CDN域名，最后要有：‘/’
+//   accessKey: 'xxxxxxx',//AK
+//   secretKey: 'xxxxxx',//SK
+//   bucket: 'fe-proxtassets',//Bucket
+//   zone: 'Zone_z1',//华北
+//   cover: true
+// };
 module.exports = {
-  publicPath: 'https://xxxxx.com/', //CDN域名，最后要有：‘/’
-  accessKey: 'xxxxxxx',//AK
-  secretKey: 'xxxxxx',//SK
+  publicPath: 'https://proxt-cdn.xuetangx.com/', //CDN域名，最后要有：‘/’
+  accessKey: 'XbBjw6RmDf-8tTJ8vwVjI6fR1sOboimKSfnRK6Jf',//AK
+  secretKey: '_7ZPARAS--B5en3Y7qgT6f7XNemQCbR5iybLruqo',//SK
   bucket: 'fe-proxtassets',//Bucket
   zone: 'Zone_z1',//华北
   cover: true
 };
+// module.exports = {
+//   publicPath: 'https://static-cdn.xuetangx.com/', //CDN域名，最后要有：‘/’
+//   accessKey: 'XbBjw6RmDf-8tTJ8vwVjI6fR1sOboimKSfnRK6Jf',//AK
+//   secretKey: '_7ZPARAS--B5en3Y7qgT6f7XNemQCbR5iybLruqo',//SK
+//   bucket: 'xtassets',//Bucket
+//   zone: 'Zone_z1',//华北
+//   cover: true
+// };
